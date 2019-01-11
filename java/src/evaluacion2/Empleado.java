8b98770204adce224b9b2f34aadd2a892737d4d9
AmaroMartinez <amaromartinezangulo@gmail.com>
2019-01-11 14:17:30 +0100

    VERSION 11/1/2019 V1

---
+++ java/src/evaluacion2/Empleado.java
@@ -0,0 +1,140 @@
+package evaluacion2;
+
+import java.util.Scanner;
+
+public class Empleado extends Persona {
+	// Defino la clase Racional
+	// Propiedades
+	private String departamento;
+	private String puesto;
+	private Double salario;
+
+	// Metodos
+
+	// Constructor por defecto
+	public Empleado() {
+		this.departamento = "**";
+		this.puesto = "**";
+		this.salario = 0.0;
+	}
+
+	// Constructor Personalizado
+	public Empleado(String de, String p, Double s) {
+		this.departamento = de;
+		this.puesto = p;
+		this.salario = s;
+	}
+	
+	// Constructor Personalizado
+	public Empleado(String d, String n, String a, Fecha F, String de, String p, Double s) {
+		super(d,n,a,F);
+		this.departamento = de;
+		this.puesto = p;
+		this.salario = s;
+	}
+
+	// Constructor Copia
+	public Empleado(Empleado E) {
+		this.departamento = E.departamento;
+		this.puesto = E.puesto;
+		this.salario = E.salario;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return (getdni() + " " + getnombre() + " " + getapellidos() + " " + getfechanacimiento() + " " + departamento + " " + puesto + " " + salario);
+	}
+/*
+//Getters and Setters
+	public String getdni() {
+		return dni;
+	}
+
+	public void setdni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getnombre() {
+		return nombre;
+	}
+
+	public void setnombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getapellidos() {
+		return apellidos;
+	}
+
+	public void setapellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Fecha getfechanacimiento() {
+		return fechanacimiento;
+	}
+
+	public void setfechanacimiento(Fecha fechanacimiento) {
+		this.fechanacimiento = new Fecha(fechanacimiento);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellidos == null) ? 0 : apellidos.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + ((fechanacimiento == null) ? 0 : fechanacimiento.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empleado other = (Empleado) obj;
+		if (this.dni == other.dni) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public int compareTo(Empleado other) {
+		return (this.dni.compareTo(other.dni));
+	}
+
+//Leer
+	public void leer(Scanner teclado) {
+		// Lee por teclado las propiedades
+		// Leo nombre
+		System.out.println("Nombre: ");
+		this.nombre = teclado.nextLine();
+		// Leo apellidos
+		System.out.println("Apellidos: ");
+		this.apellidos = teclado.nextLine();
+		// Leo dni
+		System.out.println("DNI: ");
+		this.dni = teclado.nextLine();
+		// Leo fecha
+		
+		//System.out.println("Fecha de nacimiento");
+		//System.out.println("Dia: ");
+		//dia = teclado.nextInt();
+		//System.out.println("Mes: ");
+		//mes = teclado.nextInt();
+		//System.out.println("Año: ");
+		//año = teclado.nextInt();
+		//this.fechanacimiento = new Fecha(dia, mes, año);
+		
+		System.out.println("Fecha de nacimiento: ");
+		this.fechanacimiento.leer(teclado);
+
+	}*/
+}
---
+++ java/src/evaluacion2/EmpleadoMain.java
@@ -0,0 +1,54 @@
+package evaluacion2;
+
+import java.util.Scanner;
+
+public class EmpleadoMain {
+
+	public static void main(String[] args) {
+		// Clase para probar la clase Racional
+		Empleado E1 = new Empleado();
+		System.out.println(E1);
+		Empleado E2 = new Empleado("depar","pue",5.5);
+		System.out.println(E2);
+		Empleado E3 = new Empleado("11111111A","Amaro","Martinez Angulo",new Fecha(),"Ventas","Subnormal",1.5);
+		System.out.println(E3);
+		Empleado E4 = new Empleado(E2);
+		System.out.println(E4);
+		/*
+	// Probar el Getter and Setters
+				P2.setnombre("A"); // 
+				P2.setapellidos("A"); // 
+				P3.setdni("A"); //  
+				Fecha F1 = new Fecha (1,1,1);
+				P3.setfechanacimiento(F1); // 
+				System.out.println("Modificado: " + P2);
+				System.out.println("Modificado: " + P3);
+		
+	//Probar Hash and equals
+		if (P3.equals(P2)) {
+			System.out.println(P3 + " y " + P2 + " Son iguales");
+		}
+		else {
+			System.out.println(P3 + " y " + P2 + " No son iguales");
+		}
+		
+	// CompareTo
+				if (P3.compareTo(P2)>0) {
+					System.out.println(P3 + " es mayor que " + P2);
+				}
+				else if (P3.compareTo(P2)<0) {
+					System.out.println(P3 + " es menor que " + P2);
+				}
+				else {
+					System.out.println(P3 + " es igual que " + P2);
+				}
+		
+			// Leer
+				Scanner teclado = new Scanner(System.in);
+				P1.leer(teclado);
+				teclado.close();
+				System.out.println(P1);
+				*/
+	}
+
+}
